f9296392937330a9377a08fd76ef5bb3c024ccc5
junes7 <2junes@naver.com>
2020-12-17 18:15:43 +0900

    python four fundamental rules of arithmetics

--- practice1.py
+++ practice1.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+print('Hello, world!')
+print('Python Programming')
+a = 2 + 2 # 더하기
+if a == 3:
+    print(a)
+else:
+    print('3이')
+    print('아닙니다.')
+# 사칙연산
+b = 5 // 2
+c = 5 / 2
+d = 5 % 2
+e = 5 ** 2
+print(b, c, d, e)
