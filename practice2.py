8738fb04970ee4507ddaaecdf49351f4134b893e
junes7 <2junes@naver.com>
2020-12-22 23:47:47 +0900

    study split and map function with input method

--- practice2.py
+++ practice2.py
@@ -13,3 +13,16 @@ print(a + b)
 a = int(input('첫 번째 숫자를 입력하세요: '))
 b = int(input('두 번째 숫자를 입력하세요: '))
 print(a + b)
+# 입력받은 값을 공백을 기준으로 분리해
+# 변수 두 개에 저장하기
+a, b = input('문자열 두 개를 입력하세요: ').split()
+print(a)
+print(b)
+# 입력받은 값을 공백을 기준으로 분리
+a, b = input('숫자 두 개를 입력하세요: ').split()
+a = int(a)
+b = int(b)
+print(a + b)
+# map 함수를 사용해 한 번에 여러 입력 문자열 정수형으로 변환
+a, b = map(int, input('숫자 두 개를 입력하세요: ').split())
+print(a + b)
