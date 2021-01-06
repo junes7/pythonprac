b643cd751d195fb470c39d2f5afb95289b7391c8
Jun Sung Lee <2junes@naver.com>
2021-01-07 01:49:10 +0900

    study nested if statement and logical operator with python language

--- practice8.py
+++ practice8.py
@@ -6,3 +6,28 @@ if coupon == 'Cash3000':
 if coupon == 'Cash5000':
     price -= 5000
 print(price)
+
+x = 10
+y = 20
+if x == 10 and y == 20:
+    print(True)
+else:
+    print(False)
+
+# 연습문제: 합격 여부 판단하기
+written_test = 75
+coding_test = True
+if written_test >= 80 and coding_test == True:
+	print('합격')
+else:
+   	print('불합격')
+# 심사문제: 합격 여부 판단하기
+korean, english, math, science = map(int, input().split())
+if (0 <= korean <= 100) and (0 <= english <= 100) and (0 <= math <= 100) and (0 <= science <= 100):
+    avg = (korean + english + math + science)/4.0
+    if avg >= 80:
+        print('합격')
+    else:
+        print('불합격')
+else:
+    print('잘못된 점수')
