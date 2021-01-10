f69d285a7a5f4c9495f2d6e2f772e5f81eb5c460
Jun Sung Lee <2junes@naver.com>
2021-01-11 00:25:02 +0900

    print number which not ends 3 between two input numbers

--- practice9.py
+++ practice9.py
@@ -48,7 +48,19 @@ while True:
     if i % 10 != 3:
         i += 1
         continue
-    if i > 74:  
+    if i > 73:  
         break
+    print(i, end=' ')
+    i += 1
+print('\n')
+# 심사문제: 두 수 사이의 숫자 중 3으로 끝나지 않는 숫자 출력하기
+start, stop = map(int, input().split())
+i = start
+while True:
+    if i > stop:
+    	break
+    if i % 10 == 3:
+        i += 1
+        continue
     print(i, end=' ')
     i += 1
