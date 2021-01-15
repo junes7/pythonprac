026206f2070b06e83c337f582410f46b942ddc21
Jun Sung Lee <2junes@naver.com>
2021-01-15 21:34:27 +0900

    print common multiple of three and five without using logical operator by using for loop and if, elif, else statement with python

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -23,4 +23,14 @@ for i in range(1, 101):
     else:
         print(i)
 print()
-
+# 논리 연산자를 사용하지 않고 3과 5의 공배수 처리하기
+for i in range(1, 101):
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+print()
