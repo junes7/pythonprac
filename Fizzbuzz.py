149d5200b2fd29dcd143c217f6c0aba1b539fa26
Jun Sung Lee <2junes@naver.com>
2021-01-15 17:07:50 +0900

    print common multiple of three and five between 1 to 100 by using for loop and if, elif, else statement with python

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -13,3 +13,14 @@ for i in range(1, 101):
         print(i)
 print()
 # 1부터 100까지 숫자 중 3과 5의 공배수 처리하기
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+print()
+
