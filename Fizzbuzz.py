86a63ceea990fb6ce7e6267aa56aa9b6f2362c04
Jun Sung Lee <2junes@naver.com>
2021-01-15 16:57:06 +0900

    print multiple of 3 and multiple of 5 between 1 to 100 number by using for loop and if, elif, else statement with python language

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -4,5 +4,12 @@ for i in range(1, 101):
 print()
 # 1부터 100까지 숫자 중 3의 배수일 때와 5의 배수일 때
 # 숫자 대신 'Fizz', 'Buzz' 출력하도록 처리하기
-
+for i in range(1, 101):
+    if i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+print()
 # 1부터 100까지 숫자 중 3과 5의 공배수 처리하기
