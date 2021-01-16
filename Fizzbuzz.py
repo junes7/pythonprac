90043d8fdca430a50f31965bd92df4bc81b00bd2
Jun Sung Lee <2junes@naver.com>
2021-01-16 23:43:29 +0900

    print common multiple of two and eleven by using for loop and if, else statement and range method with python

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -49,3 +49,18 @@ for i in range(1, 101):
     else:
         print(i)
 print('-------------------')
+
+# 연습문제: 2와 11의 배수, 공배수 처리하기
+# 1부터 100까지 숫자를 출력하면서 2의 배수일 때는 'Fizz',
+# 11의 배수일 때는 'Buzz', 2과 11의 공배수일 때는 'FizzBuzz'가
+# 출력되게 만드세요.
+for i in range(1, 101):
+    if i % 2 == 0 and i % 11 == 0:
+        print('FizzBuzz')
+    elif i % 2 == 0:
+        print('Fizz')
+    elif i % 11 == 0:
+        print('Buzz')
+    else:
+        print(i)
+print()
