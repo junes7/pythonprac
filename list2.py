72d902940b0580227e6856b1ecad5fb294d08aeb
Jun Sung Lee <2junes@naver.com>
2021-01-22 23:45:17 +0900

    study how to use for loop and if statement many times in list comprehension with python

--- list2.py
+++ list2.py
@@ -15,3 +15,8 @@ print(a)
 # 0~9 숫자 중 홀수에 5를 더하여 리스트 생성
 b = [i + 5 for i in range(10) if i % 2 ==1]
 print(b)
+# 반복문과 if 조건문을 여러 번 사용하기
+# 2단부터 9단까지 구구단을 리스트 생성합니다.
+a = [i * j for j in range(2, 10)
+           for i in range(1, 10)]
+print(a)
