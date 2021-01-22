ea278510ce159c745154ddca10227e4e598b54f0
Jun Sung Lee <2junes@naver.com>
2021-01-22 23:32:52 +0900

    study how to use if statement on list comprehension by using list and for loop with python

--- list2.py
+++ list2.py
@@ -8,3 +8,10 @@ print(c)
 # 0부터 9까지 숫자를 생성하면서 값에 2를 곱하여 리스트 생성
 d = list(i*2 for i in range(10))
 print(d)
+# 리스트 표현식에서 if조건문 사용하기
+# 0~9 숫자 중 2의 배수인 숫자(짝수)로 리스트 생성
+a = [i for i in range(10) if i % 2 ==0]
+print(a)
+# 0~9 숫자 중 홀수에 5를 더하여 리스트 생성
+b = [i + 5 for i in range(10) if i % 2 ==1]
+print(b)
