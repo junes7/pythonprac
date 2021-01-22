8274aadb9908d0d7ce79d77ff0099a9766e70348
Jun Sung Lee <2junes@naver.com>
2021-01-22 23:57:55 +0900

    study how to use map method in list comprehension with python

--- list2.py
+++ list2.py
@@ -20,3 +20,12 @@ print(b)
 a = [i * j for j in range(2, 10)
            for i in range(1, 10)]
 print(a)
+# 실수가 저장된 리스트가 있을 때 이 리스트의 모든 요소를 정수로 변환
+a = [1.2, 2.5, 3.7, 4.6]
+print('정수 변환 전:', a)
+# for i in range(len(a)):
+#     # 실수를 정수로 변환
+#     a[i] = int(a[i])
+# map을 이용해 실수를 정수로 변환
+a = list(map(int, a))
+print('정수 변환 후:', a)
