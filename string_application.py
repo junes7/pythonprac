f9895443a99f2832b074027884a8a61eb687dce5
Jun Sung Lee <2junes@naver.com>
2021-01-25 00:31:44 +0900

    create three dimensional list by using for loop and list and range method with python

---
+++ string_application.py
@@ -0,0 +1,30 @@
+# 문자열 바꾸기
+# print('Hello, world!'.replace('world', 'Python'))
+# s = 'Hello, world!'
+# s = s.replace('world', 'Python')
+# print(s)
+
+# 문자 바꾸기
+# table = str.maketrans('aeiou', '12345')
+# print('apple'.translate(table))
+
+# 문자열 분리하기
+# print('apple pear graph pineapple orange'.split())
+# 기준 문자열이 ,(콤마)와 공백일 때
+# print('apple, pear, graph, pineapple, orange'.split(', '))
+
+# 구분자 문자열과 문자열 리스트 연결하기
+# 구분자 문자열이 공백일 때
+# print(' '.join(['apple', 'pear', 'graph', 'pineapple', 'orange']))
+# 구분자 문자열이 -(하이픈)일 때
+# print('-'.join(['apple', 'pear', 'graph', 'pineapple', 'orange']))
+
+# 소문자를 대문자로 바꾸기
+# print('python'.upper())
+
+# 대문자를 소문자로 바꾸기
+# print('PYTHON'.lower())
+
+
+
+
--- two_dimensional_list.py
+++ two_dimensional_list.py
@@ -92,3 +92,71 @@ print(b)
 # 리스트 표현식을 활용해 톱니형 리스트 만들기
 a = [[0] * i for i in a]
 print(a)
+
+students = [
+    ['john', 'C', 19],
+    ['maria', 'A', 25],
+    ['andrew', 'B', 7]
+]
+# 안쪽 리스트의 인덱스 1을 기준으로 정렬
+print(sorted(students, key = lambda student: student[1]))
+# 안쪽 리스트의 인덱스 2을 기준으로 정렬
+print(sorted(students, key = lambda student: student[2]))
+# c = [[0 for j in range(3)] for i in range(3)]
+# c = [[0, 0, 0] for i in range(3)]
+c = [0 for i in range(3)]
+d = [[[0] for j in range(3)] for i in range(3)]
+e = [[0, 0, 0] * 3]
+print(c)
+print(d)
+print(e)
+
+# 2차원 리스트의 할당과 복사 알아보기
+a = [[10, 20], [30, 40]]
+b = a
+b[0][0] = 500
+print(a)
+print(b)
+
+# 2차원 이상의 다차원 리스트를 완전히 복사하려면
+# copy대신 copy모듈의 deepcopy함수를 사용해야 합니다.
+import copy
+a = [[10, 20], [30, 40]]
+b = copy.deepcopy(a)
+b[1][0] = 150
+print(a)
+print(b)
+
+# 연습문제: 3차원 리스트 만들기
+# 높이(depth) 2, 세로 크기(row) 4, 가로 크기(col) 3인 3차원 리스트 만들기
+# int를 사용하려면 range method를 사용할 것
+a = [[[0 for col in range(3)] for row in range(4)] for depth in range(2)]
+print(a)
+
+# # 심사문제: 지뢰찾기(Minesweeper)
+# col, row = map(int, input().split())
+# matrix = []
+# for i in range(row):
+#     matrix.append(list(input()))
+# print('입력 결과 확인:')
+# # 입력 결과 확인
+# for i in matrix:
+#     for j in i:
+#         print(j, end='')
+#     print()
+# print('값 변경 후 출력:')
+# # 값 변경하여 출력하기
+# for i in range(row):
+#     for j in range(col):
+#         if matrix[i][j] == '*':
+#             print(matrix[i][j], sep='', end='')
+#         else:
+#             count = 0
+#             for x in range(i-1, i+2):
+#                 for y in range(j-1, j+2):
+#                     if x < 0 or y < 0 or x >= row or y >= col:
+#                         continue 
+#                     elif matrix[x][y] == '*':
+#                         count += 1
+#             print(count, sep='', end='')
+#     print()
