1777bc26c55a6ccabf6d0ceab06b4ab5b07d7236
junes7 <2junes@naver.com>
2021-01-30 23:24:54 +0900

    study how to express decimal point by using format specifier

--- string_application2.py
+++ string_application2.py
@@ -11,3 +11,13 @@ print('I am %s.' % 'james')
 # input string by using format specifier with variable
 name = 'david'
 print('I am %s.' % name)
+
+# input number in string by using format speicifier
+print('I am %d years old.' % 20)
+
+# express decimal point by using format specifier
+print('%f' % 2.3)
+
+
+
+
