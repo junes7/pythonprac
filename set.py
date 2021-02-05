312c27ccd933cafed081162a37fbf6fc8a1e5ef2
junes7 <2junes@naver.com>
2021-02-05 23:45:17 +0900

    study how to check special value in set bundle with python

--- set.py
+++ set.py
@@ -5,3 +5,8 @@ print(fruits)
 # set를 사용하여 세트 만들기
 a = set('apple')
 print(a)
+
+# 세트에 특정 값이 있는지 확인하기
+fruits = {'strawberry', 'grape', 'orange', 'pineapple', 'cherry'}
+print('orange' in fruits)
+print('peach' in fruits)
