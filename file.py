25756a7749837ed150a504ddc6c11e17005dd754
junes7 <2junes@naver.com>
2021-02-11 23:47:18 +0900

    study how to write to text file and how to read string from text file

---
+++ file.py
@@ -0,0 +1,8 @@
+file = open('hello.txt', 'w')    # hello.txt 파일을 쓰기 모드(w)로 열기. 파일 객체 반환
+file.write('Hello, world!')      # 파일에 문자열 저장
+file.close()                     # 파일 객체 닫기
+
+file = open('hello.txt', 'r')    # hello.txt 파일을 읽기 모드(r)로 열기. 파일 객체 반환
+s = file.read()                  # 파일에서 문자열 읽기
+print(s)                         # Hello, world!
+file.close()                     # 파일 객체 닫기
