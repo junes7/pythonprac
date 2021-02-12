246e4c6c57588907276ba572c51abbfb34dce432
junes7 <2junes@naver.com>
2021-02-12 23:26:46 +0900

    read file contents for line in file by using for loop

--- file.py
+++ file.py
@@ -12,3 +12,8 @@ with open('hello.txt', 'r') as file:    # hello.txt 파일을 읽기 모드(r)�
     s = file.read()                     # 파일에서 문자열 읽기
     print(s)                            # Hello, world!
 
+
+# for 반복문으로 파일의 내용을 줄 단위로 읽기
+with open('hello.txt', 'r') as file:    # hello.txt 파일을 읽기 모드(r)로 열기
+    for line in file:    # for에 파일 객체를 지정하면 파일의 내용을 한 줄씩 읽어서 변수에 저장함
+        print(line.strip('\n'))    # 파일에서 읽어온 문자열에서 \n 삭제하여 출력ㄹ
