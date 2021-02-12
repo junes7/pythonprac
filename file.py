3dceff7c2565657e1a292e75fd379b8e17b7f769
junes7 <2junes@naver.com>
2021-02-12 23:37:32 +0900

    print N-gram by using for loop

--- file.py
+++ file.py
@@ -16,4 +16,10 @@ with open('hello.txt', 'r') as file:    # hello.txt 파일을 읽기 모드(r)�
 # for 반복문으로 파일의 내용을 줄 단위로 읽기
 with open('hello.txt', 'r') as file:    # hello.txt 파일을 읽기 모드(r)로 열기
     for line in file:    # for에 파일 객체를 지정하면 파일의 내용을 한 줄씩 읽어서 변수에 저장함
-        print(line.strip('\n'))    # 파일에서 읽어온 문자열에서 \n 삭제하여 출력ㄹ
+        print(line.strip('\n'))    # 파일에서 읽어온 문자열에서 \n 삭제하여 출력
+
+# 반복문으로 N-gram 출력하기
+text = 'Hello'
+ 
+for i in range(len(text) - 1):             # 2-gram이므로 문자열의 끝에서 한 글자 앞까지만 반복함
+    print(text[i], text[i + 1], sep='')    # 현재 문자와 그다음 문자 출력
