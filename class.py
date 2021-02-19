3bead372196c724a9616a06719fe99db381e0e3b
junes7 <2junes@naver.com>
2021-02-19 22:54:17 +0900

    study how to get value when we made instance of class

--- class.py
+++ class.py
@@ -42,4 +42,20 @@ class Person:
 james = Person()
 james.greeting()	# 안녕하세요.
 
+# 인스턴스를 만들 때 값 받기
+class Person:
+    def __init__(self, name, age, address):
+        self.hello = '안녕하세요.'
+        self.name = name
+        self.age = age
+        self.address = address
+        
+	def greeting(self):
+        print('{0} 저는 {1} 입니다.'.format(self.hello, self.name))
+        
+maria = Person('마리아', 20, '서울시 서초구 반포동')
+maria.greeting() # 안녕하세요. 저는 마리아입니다.
+print('이름:', maria.name)	# 마리아
+print('나이:', maria.age)		# 20
+print('주소:', maria.address)	# 서울시 서초구 반포동
 
