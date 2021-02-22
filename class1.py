d705b0ce6e876a966ad4b3a0fdd8116df9108a47
junes7 <2junes@naver.com>
2021-02-22 19:23:21 +0900

    study instance method and static method of python data type with update and set.union method

--- class1.py
+++ class1.py
@@ -1,15 +1,15 @@
 # 비공개 클래스 속성 사용하기
-class Knight:
-    __item_limit = 10    # 비공개 클래스 속성
+# class Knight:
+#     __item_limit = 10    # 비공개 클래스 속성
  
-    def print_item_limit(self):
-        print(Knight.__item_limit)    # 클래스 안에서만 접근할 수 있음
+#     def print_item_limit(self):
+#         print(Knight.__item_limit)    # 클래스 안에서만 접근할 수 있음
  
  
-x = Knight()
-x.print_item_limit()    # 10
+# x = Knight()
+# x.print_item_limit()    # 10
  
-print(Knight.__item_limit)    # 클래스 바깥에서는 접근할 수 없음
+# print(Knight.__item_limit)    # 클래스 바깥에서는 접근할 수 없음
 
 # 클래스와 메서드의 독스트링 사용하기
 class Person:
@@ -38,3 +38,8 @@ class Calc:
 Calc.add(10, 20)    # 클래스에서 바로 메서드 호출
 Calc.mul(10, 20)    # 클래스에서 바로 메서드 호출
 
+# 파이썬 자료형의 인스턴스 메서드와 정적 메서드
+a = {1, 2, 3, 4}
+a.update({5}) # 인스턴스 메서드
+print(a)
+print(set.union({1, 2, 3, 4}, {5}))
