7ef357b60127a256493593cb27f076037ac5fc80
junes7 <2junes@naver.com>
2021-02-24 23:46:33 +0900

    study how to use abstract class

--- class_inheritance.py
+++ class_inheritance.py
@@ -107,3 +107,22 @@ x.greeting()    # 안녕하세요. B입니다.
 D.mro()
 x = D()
 x.greeting()    # 안녕하세요. B입니다.
+
+# 추상 클래스 사용하기
+from abc import *
+ 
+class StudentBase(metaclass=ABCMeta):
+    @abstractmethod
+    def study(self):
+        pass
+ 
+    @abstractmethod
+    def go_to_school(self):
+        pass
+ 
+class Student(StudentBase):
+    def study(self):
+        print('공부하기')
+ 
+james = Student()
+james.study()
