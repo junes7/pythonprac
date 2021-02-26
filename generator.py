a0c35d3119f3230696ac506e73b708a0c6871702
junes7 <2junes@naver.com>
2021-02-26 23:58:42 +0900

    study how to pass out a value multiple times with yield from

--- generator.py
+++ generator.py
@@ -25,4 +25,10 @@ def number_generator():
  
 for i in number_generator():
     print(i)
-    
+
+def number_generator():
+    x = [1, 2, 3]
+    yield from x    # 리스트에 들어있는 요소를 한 개씩 바깥으로 전달
+ 
+for i in number_generator():
+    print(i)
