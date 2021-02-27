425322093bba0a1c73d1cfa6d43c5cad2e205964
junes7 <2junes@naver.com>
2021-02-27 23:47:29 +0900

    appoint generator object in yield from

--- generator.py
+++ generator.py
@@ -39,3 +39,18 @@ next(g)
 next(g)
 next(g)
 
+# yield from에 제너레이터 객체 지정하기
+def number_generator(stop):
+    n = 0
+    while n < stop:
+        yield n
+        n += 1
+        
+def three_generator():
+    # 숫자를 세 번 바깥으로 전달
+    yield from number_generator(3)
+    
+for i in three_generator():
+    print(i)
+
+
