5653fd290b0ce0801f23e9baac8c3ee4a23f3a2a
junes7 <2junes@naver.com>
2021-03-02 11:39:10 +0900

    study and review how to use decorators with methods

--- coroutine2.py
+++ coroutine2.py
@@ -154,3 +154,19 @@ def get_min(**kwargs):   # 키워드 인수를 사용하는 가변 인수 함수
  
 print(get_max(10, 20))
 print(get_min(x=10, y=20, z=30))
+
+# 메서드에 데코레이터 사용하기
+def trace(func):
+    def wrapper(self, a, b):   # 호출할 함수가 인스턴스 메서드이므로 첫 번째 매개변수는 self로 지정
+        r = func(self, a, b)   # self와 매개변수를 그대로 넣어줌
+        print('{0}(a={1}, b={2}) -> {3}'.format(func.__name__, a, b, r))   # 매개변수와 반환값 출력
+        return r               # func의 반환값을 반환
+    return wrapper
+ 
+class Calc:
+    @trace
+    def add(self, a, b):    # add는 인스턴스 메서드
+        return a + b
+ 
+c = Calc()
+print(c.add(10, 20))
