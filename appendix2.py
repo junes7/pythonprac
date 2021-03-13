06d09276d29d4c00ed97699e6f2708d7a715a8c8
junes7 <2junes@naver.com>
2021-03-13 23:32:04 +0900

    study how to determine if two real numbers are the same

--- appendix2.py
+++ appendix2.py
@@ -16,3 +16,12 @@ d = datetime.datetime(2021, 3, 13)
 from datetime import timedelta
 print(d - timedelta(days=20))
 print(datetime.datetime(2021, 3, 13) - datetime.datetime(2021, 2, 1))
+
+# 두 실수가 같은지 판단하기
+import math, sys
+x = 0.1 + 0.2
+print(math.fabs(x - 0.3) <= sys.float_info.epsilon)
+# 파이썬 3.5 이상부터는 math.isclose함수를 사용해 두 실수가 같은지 판단
+print(math.isclose(0.1 + 0.2, 0.3))
+
+
