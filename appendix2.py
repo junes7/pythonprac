9d2812fb4bf9cbd8efd3422f9087d6ef29dd07b5
junes7 <2junes@naver.com>
2021-03-14 23:59:19 +0900

    study how to create native coroutine

--- appendix2.py
+++ appendix2.py
@@ -78,5 +78,12 @@ a = Hello()     # 클래스 Hello로 인스턴스 a 생성
 b = Hello()     # 클래스 Hello로 인스턴스 b 생성
 print(a is b)   # True: 인스턴스 a와 b는 같음
 
-
-
+# 네이티브 코루틴 만들기
+import asyncio
+ 
+async def hello():    # async def로 네이티브 코루틴을 만듦
+    print('Hello, world!')
+ 
+loop = asyncio.get_event_loop()     # 이벤트 루프를 얻음
+loop.run_until_complete(hello())    # hello가 끝날 때까지 기다림
+loop.close()                        # 이벤트 루프를 닫음
