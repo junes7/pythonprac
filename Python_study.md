9a6b38e4cf2d61179919f83e9dd93051c0099853
junes7 <2junes@naver.com>
2021-03-14 23:57:39 +0900

    study how to utilize metaclass

--- Python_study.md
+++ Python_study.md
@@ -10465,6 +10465,28 @@ print(c.add(1, 2))                 # 3: 인스턴스 c의 메서드 호출
 
 
 
+### 메타클래스 활용하기(Utilize meta-class)
+
+* 메타클래스는 주로 클래스의 동작을 제어할 때 사용합니다. 다음은 싱글톤(Singleton)이라는 방식인데 클래스의 인스턴스를 언제나 하나만 생성해냅니다.
+
+```python
+class Singleton(type):    # type을 상속받음
+    __instances = {}      # 클래스의 인스턴스를 저장할 속성
+    def __call__(cls, *args, **kwargs):    # 클래스로 인스턴스를 만들 때 호출되는 메서드
+        if cls not in cls.__instances:     # 클래스로 인스턴스를 생성하지 않았는지 확인
+            cls.__instances[cls] = super().__call__(*args, **kwargs)
+                                           # 생성하지 않았으면 인스턴스를 생성하여 속성에 저장
+        return cls.__instances[cls]        # 클래스로 인스턴스를 생성했으면 인스턴스 반환
+ 
+class Hello(metaclass=Singleton):    # 메타클래스로 Singleton을 지정
+    pass
+ 
+a = Hello()     # 클래스 Hello로 인스턴스 a 생성
+b = Hello()     # 클래스 Hello로 인스턴스 b 생성
+print(a is b)   # True: 인스턴스 a와 b는 같음
+
+```
+
 
 
 
--- appendix2.py
+++ appendix2.py
@@ -61,3 +61,22 @@ Calc = MakeCalc('Calc', (), {})    # 메타클래스 MakeCalc로 클래스 Calc 
 c = Calc()                         # 클래스 Calc로 인스턴스 c 생성
 print(c.desc)                      # '계산 클래스': 인스턴스 c의 속성 출력
 print(c.add(1, 2))                 # 3: 인스턴스 c의 메서드 호출
+
+# 메타클래스 활용하기
+class Singleton(type):    # type을 상속받음
+    __instances = {}      # 클래스의 인스턴스를 저장할 속성
+    def __call__(cls, *args, **kwargs):    # 클래스로 인스턴스를 만들 때 호출되는 메서드
+        if cls not in cls.__instances:     # 클래스로 인스턴스를 생성하지 않았는지 확인
+            cls.__instances[cls] = super().__call__(*args, **kwargs)
+                                           # 생성하지 않았으면 인스턴스를 생성하여 속성에 저장
+        return cls.__instances[cls]        # 클래스로 인스턴스를 생성했으면 인스턴스 반환
+ 
+class Hello(metaclass=Singleton):    # 메타클래스로 Singleton을 지정
+    pass
+ 
+a = Hello()     # 클래스 Hello로 인스턴스 a 생성
+b = Hello()     # 클래스 Hello로 인스턴스 b 생성
+print(a is b)   # True: 인스턴스 a와 b는 같음
+
+
+
