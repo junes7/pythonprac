8fd630b99549870b8398247a91485a949a7b84ae
junes7 <2junes@naver.com>
2021-03-16 23:56:03 +0900

    study how to use properties with class definition

--- appendix4.py
+++ appendix4.py
@@ -49,7 +49,18 @@ loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
 loop.close()
 
-# Windows에서 가상 환경 만들기
-
-
+# 프로퍼티 사용하기
+class Person:
+    def __init__(self):
+        self.__age = 0
+ 
+    def get_age(self):           # getter
+        return self.__age
+    
+    def set_age(self, value):    # setter
+        self.__age = value
+ 
+james = Person()
+james.set_age(20)
+print(james.get_age())
 
