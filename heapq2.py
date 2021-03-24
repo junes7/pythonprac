78f591d446f16d30bd3f1105ccf2870c5491aac3
junes7 <2junes@naver.com>
2021-03-24 23:51:08 +0900

    review how to figure heapsort with for loop

--- heapq2.py
+++ heapq2.py
@@ -1,4 +1,3 @@
-import heapq
 def heapsort(iterable):
     h = []
     result = []
@@ -9,6 +8,3 @@ def heapsort(iterable):
     for i in range(len(h)):
         result.append(heapq.heappop(h))
     return result
-
-result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
-print(result)
