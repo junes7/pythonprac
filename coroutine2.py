8c1d34d0431270e16da56fadb5a535808143ee1a
junes7 <2junes@naver.com>
2021-03-24 23:52:30 +0900

    review how to use coroutine function

--- coroutine2.py
+++ coroutine2.py
@@ -48,7 +48,6 @@ for i in range(1, 101):   # 1부터 100까지 반복
     co.send(i)            # 코루틴 accumulate에 숫자를 보냄
 co.send(None)             # 코루틴 accumulate에 None을 보내서 숫자 누적을 끝냄
 
-
 def find(word):
     result = False
     while True:
@@ -61,5 +60,5 @@ next(f)
 print(f.send('Hello, Python!'))
 print(f.send('Hello, world!'))
 print(f.send('Python Script'))
- 
+print(f.send("Hello, python!"))
 f.close()
