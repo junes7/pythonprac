0fa728d1a50ad2e87db72315ed5e1eaf04b3c29d
junes7 <2junes@naver.com>
2021-03-25 23:59:32 +0900

    review how to process multiple of 3 and 10 the range from 1 to 100

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -55,11 +55,11 @@ for i in range(1, 101):
     else:
         print(i)
 print()
-# 1부터 100까지 숫자 중 5와 10의 공배수 처리하기
+# 1부터 100까지 숫자 중 3와 10의 공배수 처리하기
 for i in range(1, 101):
-    if i % 5 == 0 and i % 10 == 0:
+    if i % 3 == 0 and i % 10 == 0:
         print('FizzBuzz')
-    elif i % 5 == 0:
+    elif i % 3 == 0:
         print('Fizz')
     elif i % 10 == 0:
         print('Buzz')
